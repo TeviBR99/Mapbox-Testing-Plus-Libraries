718f96a8329e6d97618a799bb0e31577ddde3c16
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2024-06-30 13:36:53 +0200

    Added initial map

--- mapbox-plus-fun/src/app/app.module.ts
+++ mapbox-plus-fun/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoidGV2aWJyIiwiYSI6ImNseTFndWswaDAwamkycXM3Nnowemx2MmcifQ.KJDulCaawptiKUB30dBE1w'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
