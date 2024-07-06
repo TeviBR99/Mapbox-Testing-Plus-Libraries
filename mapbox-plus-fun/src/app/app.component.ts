7d5c2724bed3ce8d2ca0454a4749352f808a9927
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2024-07-06 13:24:07 +0200

    Adding variables and autocomplete styling

--- mapbox-plus-fun/src/app/app.component.ts
+++ mapbox-plus-fun/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  myControl = new FormControl('');
   options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> = new Observable<string[]>();
+
+  ngOnInit() {
+    this.search()
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  public search(){
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
 }
--- mapbox-plus-fun/src/app/app.module.ts
+++ mapbox-plus-fun/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 /* Angular Material Modules*/
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-
 
 /* Handmade components */
 import { BaseMapModule } from './base-map/base-map.module';
@@ -23,7 +23,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BaseMapModule,
     NavigationModule,
     BrowserAnimationsModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
