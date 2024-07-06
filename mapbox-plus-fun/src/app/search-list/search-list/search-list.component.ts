751fcb687d608a28864dcea19a6c51f93f1edfe5
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2024-07-06 16:08:51 +0200

    Update search-list.component.ts

--- mapbox-plus-fun/src/app/search-list/search-list/search-list.component.ts
+++ mapbox-plus-fun/src/app/search-list/search-list/search-list.component.ts
@@ -19,17 +19,17 @@ export class SearchListComponent implements OnInit{
   ngOnInit() {
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
   public search(){
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value || '')),
     );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   public loadCities(){
